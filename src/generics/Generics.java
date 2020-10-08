73000ec8d4cda5972cabdaa2ab878b74ff00c353
asbarros <anderson.barros0298@gmail.com>
2020-10-08 19:48:45 -0300

    classe reflexao

--- src/generics/Generics.java
+++ src/generics/Generics.java
@@ -3,12 +3,16 @@ package generics;
 import Controller.CategoriaController;
 import DAO.PersistenciaJPA;
 import Model.Categoria;
+import java.time.ZonedDateTime;
+import java.util.GregorianCalendar;
+import reflexao.AlunoReflectionExemplo;
+import reflexao.Reflexao;
 
 public class Generics {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
 
-        CategoriaController controller = new CategoriaController();
+        /*CategoriaController controller = new CategoriaController();
         String[] dados = new String[3];
         dados[0] = "0";
         dados[1] = "inserir";
@@ -25,9 +29,15 @@ public class Generics {
         for (String string : recupera) {
             System.out.println(string);
         }
-        controller.deletar(1);
-        
-        
+        controller.deletar(1);*/
+        final AlunoReflectionExemplo alunoReflectionExemplo = new AlunoReflectionExemplo();
+        alunoReflectionExemplo.setCodigo(01);
+        alunoReflectionExemplo.setDataMatricula(GregorianCalendar.from(ZonedDateTime.now()));
+        alunoReflectionExemplo.setNome("anderson");
+        alunoReflectionExemplo.setNumMatricula("123456789");
+        alunoReflectionExemplo.setValor(5555);
+
+        Reflexao.refletirObjeto(alunoReflectionExemplo, alunoReflectionExemplo.getClass());
 
     }
 }
--- src/reflexao/Reflexao.java
+++ src/reflexao/Reflexao.java
@@ -1,24 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reflexao;
 
 import java.lang.reflect.Field;
 
-/**
- *
- * @author hetikos
- */
 public class Reflexao {
-    public static void refletirObjeto(Object obj, Class classe){
-        System.out.println("Nome da classe: "+classe.getName());
-        System.out.println("Nome da classe (simples): "+classe.getSimpleName());
-        System.out.println("Nome da classe (canonical): "+classe.getResource(classe.getName()));
-        System.out.println("Tipo: "+classe.getTypeName());
-        System.out.println("===========================");
-        System.out.println("=========atributos=========");
-       // Field[] atributos = 
+
+    public static void refletirObjeto(Object obj, Class classe) throws IllegalArgumentException, IllegalAccessException {
+        System.out.println("Nome da classe: " + classe.getName());
+        System.out.println("Nome da classe (simples): " + classe.getSimpleName());
+        System.out.println("Nome da classe (canonical): " + classe.getResource(classe.getName()));
+        System.out.println("Tipo: " + classe.getTypeName());
+        System.out.println("=============================");
+        System.out.println("Atributos");
+        final Field[] atributos = classe.getDeclaredFields();
+        for (final Field F : atributos) {
+            F.setAccessible(true);
+            System.out.println(F.getName() + ": " + F.getType().getTypeName() + " ( primitivo: " + F.getType().isPrimitive() + ") -> " + F.get(obj));
+        }
     }
 }
